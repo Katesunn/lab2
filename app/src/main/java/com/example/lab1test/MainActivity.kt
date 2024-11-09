b4a383c88528b172650bde1556b2204bfe7f4274
Kate <solntseva_kate@mail.ru>
2024-11-09 23:29:05 +0700

    init commit

---
+++ app/src/main/java/com/example/lab1test/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.lab1test
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var epsilonInput: EditText
+    private lateinit var resultView: TextView
+    private lateinit var calculateButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        epsilonInput = findViewById(R.id.editTextNumberDecimal)
+        resultView = findViewById(R.id.resultView)
+        calculateButton = findViewById(R.id.calculateButton)
+
+        calculateButton.setOnClickListener {
+            val epsilon = epsilonInput.text.toString().toDoubleOrNull()
+
+            if (epsilon != null && epsilon > 0) {
+                val result = calculateSeriesSum(epsilon)
+                resultView.text = result
+            } else {
+                resultView.text = "Пожалуйста, введите допустимое значение для порога."
+            }
+        }
+    }
+    private fun factorial(n: Int): Double {
+        if (n == 0) {
+            return 1.0
+        } else {
+            return n * factorial(n - 1)
+        }
+    }
+    private fun calculateSeriesSum(epsilon: Double): String {
+        var sum = 0.0
+        var term: Double
+        var n = 1
+        var iterations = 0
+
+        do {
+            term = 1.0 / factorial(n)
+            sum += term
+            n += 2
+            iterations ++
+        } while(term > epsilon)
+        return "Сумма: $sum\nПоследнее слагаемое: $term\nЧисло итераций: $iterations"
+    }
+
+}
